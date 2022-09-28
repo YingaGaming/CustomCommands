58242517db51bb2662e8971c2ed37efcdb3da0ae
Xenorio <hi@xenorio.xyz>
2022-09-28 10:06:11 +0200

    add some comments

--- src/main/java/games/yinga/CustomCommands/Listeners/CommandListener.java
+++ src/main/java/games/yinga/CustomCommands/Listeners/CommandListener.java
@@ -15,11 +15,11 @@ public class CommandListener implements Listener {
     public void onCommand(PlayerCommandPreprocessEvent event) {
         FileConfiguration config = Main.getConfiguration();
 
-        config.getConfigurationSection("commands").getKeys(false).forEach(key -> {
-            if (event.getMessage().toLowerCase().split(" ")[0].equals("/" + key)) {
-                event.setCancelled(true);
-                config.getStringList("commands." + key).forEach(message -> {
-                    event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        config.getConfigurationSection("commands").getKeys(false).forEach(key -> { // Loop over every configured command
+            if (event.getMessage().toLowerCase().split(" ")[0].equals("/" + key)) { // Check if entered command matches command in config
+                event.setCancelled(true); // Prevent the default action
+                config.getStringList("commands." + key).forEach(message -> { // Loop over every response line
+                    event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message)); // Send line to player
                 });
                 
             }
